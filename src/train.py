1b467424ca5061bac8b44ac0ecc496d0f19e94da
Asad <asadakmal11@icloud.com>
2026-04-11 04:45:12 +0500

    Basic folder structure setup

---
+++ src/train.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+def train():
+    df = pd.DataFrame({
+        "x": [1,2,3,4],
+        "y": [2,4,6,8]
+    })
+
+    X = df[["x"]]
+    y = df["y"]
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    joblib.dump(model, "models/model.pkl")
+
+if __name__ == "__main__":
+    train()
---
+++ src/validate.py
@@ -0,0 +1,7 @@
+import pandas as pd
+def validate():
+    df = pd.DataFrame({"a": [1,2,3]})
+    assert not df.isnull().values.any()
+
+if __name__ == "__main__":
+    validate()
